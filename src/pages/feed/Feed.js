d91267fd6af8d769a44a0e17435162f93ee5239f
Subhadip Mandal <subhadipmandal688@gmail.com>
2023-03-09 18:21:01 +0530

    project structure

---
+++ src/pages/feed/Feed.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Leftbar from '../../components/leftbar/Leftbar'
+import Navbar from '../../components/navbar/Navbar'
+
+function Feed() {
+    return (
+        <div>
+
+            <Navbar />
+            <Leftbar />
+
+        </div>
+    )
+}
+
+export default Feed
