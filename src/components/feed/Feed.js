3d9c6463c539e194d81ef86bcd2c101c6f125c8f
Subhadip Mandal <subhadipmandal688@gmail.com>
2023-03-16 09:06:30 +0530

    post fetch bug fixed

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FetchPostAction } from "../../redux/slices/PostSlice";
 import Post from "../post/Post";
 import PostCreate from "../PostCreate/PostCreate";
 import Stories from "../stories/Stories";
 import "./Feed.css";
 
-const Feed = ({ post }) => {
+const Feed = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchPostAction());
+  }, [dispatch]);
+
+  const post = useSelector((state) => state.post?.Post);
+  console.log(post);
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Stories />
         <PostCreate />
-        {post.map((e, i) => {
+        {post ? post.map((e, i) => {
           return <Post post={e} key={i} />;
-        })}
+        }) : null}
       </div>
     </div>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,27 +1,19 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 import Feed from "../../components/feed/Feed";
 import Navbar from "../../components/navbar/Navbar";
 import Rightbar from "../../components/rightbar/Rightbar";
 import Sidebar from "../../components/sidebar/Sidebar";
-import { FetchPostAction } from "../../redux/slices/PostSlice";
+
 import "./Home.css";
 
 const Home = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(FetchPostAction());
-  }, [dispatch]);
-
-  const Post = useSelector((state) => state.post?.Post);
 
   return (
     <div className="home">
       <Navbar />
       <div className="homeContainer">
         <Sidebar />
-        {Post ? <Feed post={Post} /> : null}
-
+            <Feed/>
         <Rightbar />
       </div>
     </div>
