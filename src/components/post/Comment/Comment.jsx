6ded4b19863e6c4d0e58006a8aae89b0dc5ffc08
AryeshSaha <aryeshsaha5302@gmail.com>
2023-04-02 22:00:28 +0530

    User details are now available in Post popups due to addition of new slice in PostSlice

--- src/components/post/Comment/Comment.jsx
+++ src/components/post/Comment/Comment.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { FaTelegramPlane, FaEdit, FaTrash } from "react-icons/fa";
+import { FaTelegramPlane } from "react-icons/fa";
 
 export const Comment = ({
   commentHandler,
   comment,
   setComment,
-  Comments,
-  loading,
-  commentEditer,
-  commentDeleter,
 }) => {
   return (
     <>
@@ -23,42 +19,6 @@ export const Comment = ({
           />
           <FaTelegramPlane className="sendIcon" onClick={commentHandler} />
         </div>
-        {/* add a conditional render  */}
-        {/* {loading ? (
-          <p>Loading...</p>
-        ) : Comments.length > 0 ? (
-          Comments.map((comment) => (
-            <div key={comment.id} className="commentFuncs">
-              <div className="postBottomFooterItem">
-                <a href="/profile/userId">
-                  <img
-                    src={comment.user?.profilePhoto}
-                    alt=""
-                    className="commentProfileImg"
-                  />
-                </a>
-                <div>
-                  <small>
-                    {comment.user?.firstName} {comment.user?.lastName}
-                  </small>
-                  <p>{comment.description}</p>
-                </div>
-              </div>
-              <div className="update-delete-icons">
-                <FaEdit
-                  className="editIcon"
-                  onClick={() => commentEditer(comment.id)}
-                />
-                <FaTrash
-                  className="deleteIcon"
-                  onClick={() => commentDeleter(comment.id)}
-                />
-              </div>
-            </div>
-          ))
-        ) : (
-          <h4>Be the first one to comment</h4>
-        )} */}
       </div>
     </>
   );
